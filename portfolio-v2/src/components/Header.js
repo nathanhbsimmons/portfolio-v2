98bcc0d433f19f765563df587b24b063423fa7a7
Nathan Simmons <nathanhbsimmons@gmail.com>
2018-09-07 16:18:33 -0500

    added main header animation

---
+++ portfolio-v2/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../App.css';
+import '../Scripts.js'
+ 
+function Header(){
+  
+    return (
+        <div className="header"> 
+        
+        </div>
+    );
+  
+}
+ 
+export default Header;
